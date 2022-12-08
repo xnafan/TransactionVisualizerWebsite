24c5fb5de85e056fcf9c1da7ec99ae006903836b
Jakob Farian Krarup <kr@rup.dk>
2022-12-08 08:54:48 +0100

    UI work

--- TransactionVisualizerWebsite/Database/DatabaseAccess.cs
+++ TransactionVisualizerWebsite/Database/DatabaseAccess.cs
@@ -3,115 +3,89 @@ using System.Data.SqlClient;
 using System.Diagnostics;
 using TransactionVisualizerWebsite.Models;
 
-namespace TransactionVisualizerWebsite.Database
+namespace TransactionVisualizerWebsite.Database;
+public class DatabaseAccess
 {
-    public class DatabaseAccess
+    public bool RemoveProductIfInStock(StockUpdateParameters parameters)
     {
+        using SqlConnection? connection = CreateConnection();
+        connection.Open();
+        using SqlTransaction? transaction = connection.BeginTransaction(parameters.IsolationLevel);
+        int productStock = GetProductStock(parameters.ProductId, connection, transaction);
+        Thread.Sleep(parameters.PauseBeforeUpdateInSeconds * 1000);
+        if (productStock < parameters.StockChangeAmount) { return false; }
+        var stockAltered = AlterStock(parameters.ProductId, -parameters.StockChangeAmount, connection, transaction);
+        transaction.Commit();
+        return stockAltered;
+    }
 
-        public bool RemoveProductIfInStock(StockUpdateParameters parameters)
-        {
-            SqlConnection? connection = null;
-            SqlTransaction? transaction = null;
-
-            try
-            {
-                connection = CreateConnection();
-                connection.Open();
-                transaction = connection.BeginTransaction(parameters.IsolationLevel);
-                int productStock = GetProductStock(parameters.ProductId, connection, transaction);
-                Thread.Sleep(parameters.PauseBeforeUpdateInSeconds * 1000);
-                Debug.WriteLine(productStock);
-                if (productStock < parameters.StockChangeAmount) { return false; }
-                AlterStock(parameters.ProductId, -parameters.StockChangeAmount, connection, transaction);
-                transaction.Commit();
-                return true;
-            }
-            catch (Exception)
-            {
-                transaction?.Rollback();
-                return false;
-            }
-            finally { transaction?.Dispose();
-                connection?.Close();
-                    }
-        }
-
-        public int GetProductStock(int productId, SqlConnection? connection = null, SqlTransaction? transaction = null)
+    public int GetProductStock(int productId, SqlConnection? connection = null, SqlTransaction? transaction = null)
+    {
+        //if we didn't receive a connection create a new one
+        connection = connection ?? CreateConnection();
+        //create a command
+        SqlCommand command = connection.CreateCommand();
+        command.CommandText = "SELECT stock FROM Product WHERE Id=@productId";
+        //set the transaction (won't give any problems if null)
+        command.Transaction = transaction;
+        //set the parameter for the SQL
+        command.Parameters.AddWithValue("@productId", productId);
+        //store whether we're working on an open or closed connection
+        var initialConnectionState = connection.State;
+        //open the connection if it was closed initially
+        if (initialConnectionState == ConnectionState.Closed) { connection.Open(); }
+        //get the stock amount
+        var stock = (int)command.ExecuteScalar();
+        //close the connection if it was closed initially
+        if (initialConnectionState == ConnectionState.Closed) { connection.Close(); }
+        return stock;
+    }
+    private bool AlterStock(int productId, int change, SqlConnection? connection = null, SqlTransaction? transaction = null)
+    {
+        try
         {
             //if we didn't receive a connection create a new one
             connection = connection ?? CreateConnection();
             //create a command
             SqlCommand command = connection.CreateCommand();
-            command.CommandText = "SELECT stock FROM Product WHERE Id=@productId";
+            command.CommandText = "UPDATE Product SET Stock = Stock + @change WHERE Id=@productId";
             //set the transaction (won't give any problems if null)
-            command.Transaction = transaction;
-            //set the parameter for the SQL
+            if (transaction != null)
+            {
+                command.Transaction = transaction;
+            }
+            //set the parameters for the SQL
+            command.Parameters.AddWithValue("@change", change);
             command.Parameters.AddWithValue("@productId", productId);
             //store whether we're working on an open or closed connection
             var initialConnectionState = connection.State;
             //open the connection if it was closed initially
             if (initialConnectionState == ConnectionState.Closed) { connection.Open(); }
-            //get the stock amount
-            var stock = (int)command.ExecuteScalar();
+            //alter stock
+            var rowsAffected = command.ExecuteNonQuery();
             //close the connection if it was closed initially
             if (initialConnectionState == ConnectionState.Closed) { connection.Close(); }
-            return stock;
+            return rowsAffected > 0;
         }
-        private bool AlterStock(int productId, int change, SqlConnection? connection = null, SqlTransaction? transaction = null)
-        {
-            try
-            {
-                //if we didn't receive a connection create a new one
-                connection = connection ?? CreateConnection();
-                //create a command
-                SqlCommand command = connection.CreateCommand();
-                command.CommandText = "UPDATE Product SET Stock = Stock + @change WHERE Id=@productId";
-                //set the transaction (won't give any problems if null)
-                if (transaction != null)
-                {
-
-                    command.Transaction = transaction;
-                }
-                //set the parameters for the SQL
-                command.Parameters.AddWithValue("@change", change);
-                command.Parameters.AddWithValue("@productId", productId);
-                //store whether we're working on an open or closed connection
-                var initialConnectionState = connection.State;
-                //open the connection if it was closed initially
-                if (initialConnectionState == ConnectionState.Closed) { connection.Open(); }
-                //alter stock
-                command.ExecuteNonQuery();
-                //close the connection if it was closed initially
-                if (initialConnectionState == ConnectionState.Closed) { connection.Close(); }
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-
-        }
-
-        public void SetStock(int productId, int newStockAmount)
-        {
-            //if we didn't receive a connection create a new one
-            var connection = CreateConnection();
-            //create a command
-            SqlCommand command = connection.CreateCommand();
-            command.CommandText = "UPDATE Product SET Stock = @newStockAmount WHERE Id=@productId";
-            //set the parameters for the SQL
-            command.Parameters.AddWithValue("@newStockAmount", newStockAmount);
-            command.Parameters.AddWithValue("@productId", productId);
-
-            connection.Open();
-            //alter stock
-            command.ExecuteNonQuery();
-            //close the connection if it was closed initially
-            connection.Close();
-        }
-        private SqlConnection CreateConnection()
-        {
-            return new SqlConnection("Data Source=.; Initial Catalog=TransactionSample; Integrated Security=true;");
-        }
+        catch (Exception) { return false; }
     }
+
+    public bool SetStock(int productId, int newStockAmount)
+    {
+        using SqlConnection connection = CreateConnection();
+        //create a command
+        SqlCommand command = connection.CreateCommand();
+        command.CommandText = "UPDATE Product SET Stock = @newStockAmount WHERE Id=@productId";
+        //set the parameters for the SQL
+        command.Parameters.AddWithValue("@newStockAmount", newStockAmount);
+        command.Parameters.AddWithValue("@productId", productId);
+
+        connection.Open();
+        //alter stock
+        var rowsAffected = command.ExecuteNonQuery();
+        //close the connection if it was closed initially
+        connection.Close();
+        return rowsAffected > 0;
+    }
+    private SqlConnection CreateConnection() => new SqlConnection("Data Source=.; Initial Catalog=TransactionSample; Integrated Security=true;");
 }
--- TransactionVisualizerWebsite/Models/StockUpdateParameters.cs
+++ TransactionVisualizerWebsite/Models/StockUpdateParameters.cs
@@ -8,19 +8,33 @@ namespace TransactionVisualizerWebsite.Models
         
         public enum ValidIsolationLevels
         {
-            ReadUncommitted = IsolationLevel.ReadUncommitted, 
-            ReadCommitted = IsolationLevel.ReadCommitted, 
+            [Display(Name = "Read uncommitted")]
+            ReadUncommitted = IsolationLevel.ReadUncommitted,
+
+            [Display(Name = "Read committed")]
+            ReadCommitted = IsolationLevel.ReadCommitted,
+
+            [Display(Name = "Repeatable read")]
             RepeatableRead = IsolationLevel.RepeatableRead, 
+
             Snapshot = IsolationLevel.Snapshot, 
+
             Serializable = IsolationLevel.Serializable
         }
         [Required]
+        [Display(Name ="Product Id")]
         public int ProductId { get; set; } = 1;
+
         [Required]
+        [Display(Name = "Change in inventory")]
         public int StockChangeAmount { get; set; } = 5;
+
         [Required]
+        [Display(Name = "Pause between read and update (in seconds)")]
         public int PauseBeforeUpdateInSeconds { get; set; } = 5;
+
         [Required]
+        [Display(Name = "Transaction isolation level")]
         public IsolationLevel IsolationLevel { get; set; } = IsolationLevel.Chaos;
     }
 }
